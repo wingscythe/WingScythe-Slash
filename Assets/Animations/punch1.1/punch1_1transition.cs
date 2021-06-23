b1148bee096638bd9a524716508b173b8b98f0bd
Xurya <xur1@bxscience.edu>
2021-06-23 13:53:18 -0400

    Juicy combo integration.

---
+++ Assets/Animations/punch1.1/punch1_1transition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class punch1_1transition : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       if(PlayerController.Instance.isAttacking){
+           PlayerController.Instance.animator.Play("jabl");
+       }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       PlayerController.Instance.isAttacking = false;
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,54 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("General")]
-    [SerializeField]
-    private Animator animator;
+    public Animator animator;
     [SerializeField]
     private Rigidbody2D rb;
     private float moveInput;
     private float velocity;
     [SerializeField]
     private float speed = 1.0f;
+    public bool isAttacking = false;
+    public static PlayerController Instance;
+    public bool facingRight = false;
+
+    private void Awake(){
+        Instance = this;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Attack();
         moveInput = Input.GetAxis("Horizontal");
         if (moveInput != 0) {
             animator.SetBool("isWalking", true);
         } else {
             animator.SetBool("isWalking", false);
         }
+        if (!facingRight && moveInput > 0) {
+            Flip();
+        } else if (facingRight && moveInput < 0) {
+            Flip();
+        }
         velocity = speed * moveInput;
         rb.velocity = new Vector2(velocity,0);
         animator.SetFloat("Velocity", velocity);
+    }
+
+    void Attack(){
+        if(Input.GetKeyDown(KeyCode.X) && !isAttacking){
+            isAttacking = true;
+        }
+    }
+
+    void Flip(){
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
