2d61efc8affb3408f1077408b50dadbf0f6b3da9
AndyZheng430 <zhenga6@bxscience.edu>
2021-06-24 23:47:37 -0400

    created a gold UI

---
+++ Assets/Scripts/Display/PlayerGoldDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGoldDisplay : MonoBehaviour
+{
+    private PlayerGold gold;
+    private Text goldDisplay;
+    private void Awake() {
+        gold = GameObject.FindWithTag("Player").GetComponent<PlayerGold>();
+        goldDisplay = gameObject.GetComponent<Text>();
+    }
+
+    void Update() {
+        goldDisplay.text = "Gold: " + gold.showGold();    
+    }
+}
---
+++ Assets/Scripts/Transactions/PlayerGold.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGold : MonoBehaviour
+{
+    [SerializeField]
+    private int value = 0;
+
+    public void addGold(Gold coin)
+    {
+        value += coin.getValue();
+    }
+
+    public int showGold()
+    {
+        return value;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G)){
+            Gold g = new Gold();
+            g.setValue(10);
+            addGold(g);
+        }
+    }
+}
