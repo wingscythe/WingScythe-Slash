ba54809a112f84b90f6038d50598407dbb5fc686
AndyZheng430 <zhenga6@bxscience.edu>
2021-07-05 18:30:13 -0400

    Basic Main Menu components complete without transitions

---
+++ Assets/Scripts/Display/Menu/MenuOpener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpener : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject startFadeText;
+
+    public void OpenMenu()
+    {
+        startFadeText.SetActive(false);
+        startButton.SetActive(true);
+    }
+}
