c89c0eecc7765c99e0172d711499d9d8b95d3d26
AndyZheng430 <zhenga6@bxscience.edu>
2021-07-04 20:32:16 -0400

    working on main menu

---
+++ Assets/Scripts/Display/Menu/OpenMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    private bool isPressed = false;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isPressed)
+        {
+            ShowGUI();
+            isPressed = false;
+        }
+        // if (Input.touchCount > 0) {
+        //     ShowGUI();
+        // }
+    }
+
+    void ShowGUI()
+    {
+        Debug.Log("Screen Touched");
+    }
+}
