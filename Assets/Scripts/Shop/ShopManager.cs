e6e2bf703d21a3477ec249877f118bc2626c848e
jwengbing <jweng8@binghamton.edu>
2021-07-15 23:16:24 -0400

    Upgrade Atk Speed Test

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     private int cost;
-
+    protected int max;
+    protected int upgrades;
     public void increaseCost(int gold)
     {
         cost += gold;
+        upgrades++;
+    }
+
+    public bool maxUpgrades(int max)
+    {
+        if (upgrades == max) return true;
+        else return false;
     }
 
 }
--- Assets/Scripts/Shop/UpgradeAtkSpeed.cs
+++ Assets/Scripts/Shop/UpgradeAtkSpeed.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeAtkSpeed : ShopManager
 {
     [SerializeField]
     private PlayerGold player;
     private PlayerStat stat;
-    private int cost = 200;
+    private int cost = 100;
+    public Button button;
+    public Animator anim;
+    public UpgradeAtkSpeed()
+    {
+        this.upgrades = 0;
+        this.max = 1;
+    }
 
     //increases atkspeed in animator, change stats later
-    void AtkSpeedUp()
+    public void AtkSpeedUp()
     {
+        Debug.Log("Button pressed");
         if (player.showGold() < cost)
         {
+            Debug.Log("NOT ENOUGH GOLD" + "number of upgrades " + upgrades);
             return;
+        }
+        else if (maxUpgrades(max))
+        {
+            Debug.Log("MAX UPGRADES MADE");
+            button.GetComponentInChildren<Text>().text = "MAX UPGRADES";
         }
         else
         {
-            Animator anim = GetComponent<Animator>();
             anim.SetFloat("atkspd", anim.GetFloat("atkspd") + (float)0.5);
             player.minusGold(new Gold(cost));
-            increaseCost();
+            increaseCost(10);
         }
     }
     public void increaseCost()
@@ -30,8 +44,8 @@ public class UpgradeAtkSpeed : ShopManager
     }
 
     //remove when implementing buttons
-    void Update()
-    {
-        AtkSpeedUp();
-    }
+    /*     void Update()
+        {
+            AtkSpeedUp();
+        } */
 }
